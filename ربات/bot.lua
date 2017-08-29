c17cc3400e6388f22300172d6e925a589282b9d3
shahincat966 <31453151+shahincat966@users.noreply.github.com>
2017-08-30 03:03:36 +0430

    botgerop
    botgerop

--- ربات/bot.lua
+++ ربات/bot.lua
@@ -90,23 +90,12 @@ function create_config( )
 	"lock-fosh"
 	
  },
-    sudo_users = {123456789},
+    sudo_users = {356021711},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
     info_text = [[》permag.ir v3
 
-🔰Our channel :
-》@permag_bots《
-
-🔰Our channel2 :
-》@permag_ir《
-
-🔰Our channel3 :
-》@permag《
-
-🔰Our website :
-》http://permag.ir
 ]],
   }
   serialize_to_file(config, './data/config.lua')
